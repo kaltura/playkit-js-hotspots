77dd87b628f40bfe0ede6d38cd16dd6313dee665
Eran Sakal <eran@sakalim.com>
2019-02-25 15:45:57 +0200

    feat: support hotspots in iphone

--- src/player-v2/plugin.tsx
+++ src/player-v2/plugin.tsx
@@ -5,30 +5,73 @@ import { log, enableLog } from "../utils/logger";
 import { Hotspot } from "../utils/hotspot";
 
 function toObject(jsonAsString: string, defaultValue: { [key: string]: any } = {}): { error?: Error, result?: { [key: string]: any }} {
-	if (!jsonAsString) {
-		return defaultValue;
-	}
+  if (!jsonAsString) {
+    return defaultValue;
+  }
 
-	try {
-		return {result: JSON.parse(jsonAsString)};
-	} catch (e) {
-		return {error: e};
-	}
+  try {
+    return {result: JSON.parse(jsonAsString)};
+  } catch (e) {
+    return {error: e};
+  }
+}
+const WEBKIT_PLAYS_INLINE_KEY = 'EmbedPlayer.WebKitPlaysInline';
+
+function isIpad(){
+  return ( navigator.userAgent.indexOf('iPad') != -1 );
 }
 
+function isIphone() {
+  return ( navigator.userAgent.indexOf('iPhone') != -1 && ! isIpad() );
+}
+
+function shouldEnableIphoneMode(iphoneFullscreenSupport: boolean | undefined) : boolean
+{
+  const iphoneFullscreenSupportDefined = typeof iphoneFullscreenSupport !== 'undefined';
+
+  if (!isIphone() || (iphoneFullscreenSupportDefined && !iphoneFullscreenSupport)) {
+    return false;
+  }
+
+  return true;
+}
+
+(function setupIphoneEnvironment() {
+
+  try {
+    // @ts-ignore
+    const playerConfig = window.kalturaIframePackageData.playerConfig;
+    const inlineMode = playerConfig.vars[WEBKIT_PLAYS_INLINE_KEY];
+    const iphoneFullscreenSupport = playerConfig.plugins.hotspots.iphoneFullscreenSupport;
+    const inlineModeDefined = typeof inlineMode !== 'undefined';
+
+    log('debug', 'setupIphoneEnvironment', 'check conditions needed to setup environment', { inlineMode, inlineModeDefined, iphoneFullscreenSupport, isIphone: isIphone()});
+
+    if (inlineModeDefined || !shouldEnableIphoneMode(iphoneFullscreenSupport)) {
+      log('log','setupIphoneEnvironment', 'setup iphone environment is aborted by configuration (either iphone not detected, inline flag is already set or explicitly configured not to handle iphone fullscreen by plugin configuration)');
+      return;
+    }
+
+    log('log','setupIphoneEnvironment', 'setup iphone environment to handle use custom fullscreen handling');
+    playerConfig.vars[WEBKIT_PLAYS_INLINE_KEY] = true;
+  } catch(e) {
+    log('error','setupIphoneEnvironment', `failed to setup iphone environment with error ${e.message}`);
+  }
+}());
 
 mw.kalturaPluginWrapper(function(){
 
-	mw.PluginManager.add( 'hotspots', mw.KBaseComponent.extend( {
+  mw.PluginManager.add( 'hotspots', mw.KBaseComponent.extend( {
 
-		_root: null,
-		_videoSize: null,
+    _root: null,
+    _videoSize: null,
     _wasPlayed: false,
-		stage: null,
-		defaultConfig: {
-			parent: 'videoHolder',
-			order: 1
-		},
+    stage: null,
+    defaultConfig: {
+      parent: 'videoHolder',
+      iphoneFullscreenSupport: undefined, //important don't set it explicitly here to either false or true
+      order: 1
+    },
 
 		shouldEnableLogs() {
 			try
@@ -41,46 +84,51 @@ mw.kalturaPluginWrapper(function(){
 			}
 		},
 
-		handleVideoSizeChange:  function(e: any) {
+    handleVideoSizeChange:  function(e: any) {
       const { width, height } = this.getPlayer().evaluate("{mediaProxy.entry}");
       log('debug', 'plugin.handleVideoSizeChange', 'use entry size provided by player to calculate actual stage size', {width, height});
 
-			if (!width || !height) {
-				this._videoSize = null;
-			} else {
+      if (!width || !height) {
+        this._videoSize = null;
+      } else {
         this._videoSize = { width, height };
-			}
+      }
 
       this.stage.handleResize();
     },
 
 
     setup: function(){
-      this.shouldEnableLogs();
-			this.addBindings();
 
-		},
+      if (this.enableIphoneFullscreen()) {
+        log('log', 'setup', 'iphone detected, prevent native fullscreen and use inline player');
+        mw.setConfig('EmbedPlayer.ExternalFullScreenControl', true);
+      }
 
-		pauseVideo: function() {
+      this.addBindings();
+
+    },
+
+    pauseVideo: function() {
       this.getPlayer().sendNotification('doPause');
     },
 
-		getCuePoints: function(){
-			return this.cuePoints;
-		},
+    getCuePoints: function(){
+      return this.cuePoints;
+    },
 
-		loadCuePoints: function( callback: LoadCallback ){
-			var _this = this;
-			// do the api request
-			this.getKalturaClient().doRequest({
-					'service': 'cuepoint_cuepoint',
-					'action': 'list',
-					'filter:entryIdEqual': this.getPlayer().kentryid,
-					'filter:objectType':'KalturaCuePointFilter',
-					'filter:cuePointTypeEqual':	'annotation.Annotation',
-					'filter:tagsLike' : 'hotspots'
-				},
-				function( data: any ) {
+    loadCuePoints: function( callback: LoadCallback ){
+      var _this = this;
+      // do the api request
+      this.getKalturaClient().doRequest({
+          'service': 'cuepoint_cuepoint',
+          'action': 'list',
+          'filter:entryIdEqual': this.getPlayer().kentryid,
+          'filter:objectType':'KalturaCuePointFilter',
+          'filter:cuePointTypeEqual':	'annotation.Annotation',
+          'filter:tagsLike' : 'hotspots'
+        },
+        function( data: any ) {
           // if an error pop out:
           const hasError = !data || data.code;
 
@@ -92,12 +140,12 @@ mw.kalturaPluginWrapper(function(){
             (data.objects || []).reduce((acc: Hotspot[], cuePoint: any) => {
               const { result: partnerData, error } = toObject(cuePoint.partnerData);
               if (!partnerData || !partnerData.schemaVersion) {
-              	log('warn', 'loadCuePoints', `annotation '${cuePoint.partnerData.id}' has no schema version, skip annotation`);
+                log('warn', 'loadCuePoints', `annotation '${cuePoint.partnerData.id}' has no schema version, skip annotation`);
                 return acc;
               }
 
               const rawLayout = {
-	              ...partnerData.layout
+                ...partnerData.layout
               };
 
               acc.push({
@@ -116,52 +164,79 @@ mw.kalturaPluginWrapper(function(){
             callback({ hotspots })
           }
         }
-			);
-		},
+      );
+    },
 
-		getPlayerSize: function() {
-			const videoHolder = this.getPlayer().getVideoHolder();
-			if (!videoHolder) {
-				return null;
-			}
+    getPlayerSize: function() {
+      const videoHolder = this.getPlayer().getVideoHolder();
+      if (!videoHolder) {
+        return null;
+      }
 
-			const width = videoHolder.width();
-			const height = videoHolder.height();
+      const width = videoHolder.width();
+      const height = videoHolder.height();
 
-			return {
-				width,
-				height
-			}
-		},
+      return {
+        width,
+        height
+      }
+    },
 
-		getVideoSize: function() {
-			if (!this._videoSize) {
-				return null
-			};
+    getVideoSize: function() {
+      if (!this._videoSize) {
+        return null
+      };
 
-			return {
-				...this._videoSize
-			}
-		},
+      return {
+        ...this._videoSize
+      }
+    },
 
-		addBindings: function() {
-			var _this = this;
+    enableIphoneFullscreen: function() {
+      const iphoneFullscreenSupport = this.getConfig('iphoneFullscreenSupport');
+      return shouldEnableIphoneMode(iphoneFullscreenSupport);
+    },
 
-			this.bind( 'playerReady', function(){
+    enterFullscreenInIphone: function() {
+      if (!this.enableIphoneFullscreen()) {
+        return;
+      }
+
+      log('log', 'plugin.addBindings[onToggleFullscreen]', 'handle iphone fullscreen toggle manually');
+      const manager = this.getPlayer().layoutBuilder.fullScreenManager;
+
+      if (!manager.inFullScreen) {
+        manager.doContextTargetFullscreen();
+        manager.inFullScreen = true;
+        return;
+      }
+
+      manager.restoreContextPlayer();
+      manager.inFullScreen = false;
+    },
+
+    addBindings: function() {
+      var _this = this;
+
+      this.bind('onToggleFullscreen', function() {
+        _this.enterFullscreenInIphone();
+      });
+
+      this.bind( 'playerReady', function(){
         const props = {
           getCurrentTime: _this._getCurrentTime.bind(_this),
-					loadCuePoints: _this.loadCuePoints.bind(_this),
-					getPlayerSize: _this.getPlayerSize.bind(_this),
-					getVideoSize: _this.getVideoSize.bind(_this),
+          loadCuePoints: _this.loadCuePoints.bind(_this),
+          getPlayerSize: _this.getPlayerSize.bind(_this),
+          getVideoSize: _this.getVideoSize.bind(_this),
           pauseVideo: _this.pauseVideo.bind(_this)
-				}
+        }
 
         _this._root = render(<Stage {...props} ref={(ref) => _this.stage = ref} ></Stage>, jQuery('[id="hotspotsOverlay"]')[0]);
-			});
+      });
 
-			this.bind('updateLayout', function(){
+      this.bind('updateLayout', function(){
         _this.stage.handleResize();
-			});
+      });
 
       this.bind('firstPlay', function(){
 
@@ -169,18 +244,21 @@ mw.kalturaPluginWrapper(function(){
           _this.stage.showHotspots();
           _this._wasPlayed = true;
         }
+
+        _this.enterFullscreenInIphone();
+
       });
 
       this.bind('seeked', function(){
 
         if (!_this._wasPlayed) {
-        	_this.stage.showHotspots();
+          _this.stage.showHotspots();
           _this._wasPlayed = true;
         }
       });
 
-			this.bind('onChangeMedia', function() {
-				// DEVELOPER NOTICE: this is the destruction place.
+      this.bind('onChangeMedia', function() {
+        // DEVELOPER NOTICE: this is the destruction place.
         _this._wasPlayed = false;
         _this._videoSize = null;
 
@@ -200,22 +278,23 @@ mw.kalturaPluginWrapper(function(){
       });
 
       this.bind('seeked', function(){
-            _this.stage.notify({ type: NotifyEventTypes.Seeked });
+        _this.stage.notify({ type: NotifyEventTypes.Seeked });
       });
-		},
-
-    _getCurrentTime() {
-			return this.getPlayer().currentTime * 1000;
     },
 
-		getComponent: function () {
+    _getCurrentTime() {
+      return this.getPlayer().currentTime * 1000;
+    },
 
-			if ( ! this.$el) {
-				this.$el = jQuery( "<div></div>" ).attr( 'id', 'hotspotsOverlay' ).css({ position: 'absolute', height: '100%', width: '100%', top: 0, left: 0});
-			}
+    getComponent: function () {
 
-			return this.$el;
-		},
+      if ( ! this.$el) {
+        this.$el = jQuery( "<div></div>" ).attr( 'id', 'hotspotsOverlay' ).css({ position: 'absolute', height: '100%', width: '100%', top: 0, left: 0});
+      }
 
-	} ) );
+      return this.$el;
+    },
+
+  } ) );
 })
+
