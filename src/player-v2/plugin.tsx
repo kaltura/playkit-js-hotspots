675ed4fb186e5ef32e674409470e85e42d9fbed6
Eran Sakal <eran@sakalim.com>
2019-02-25 17:27:56 +0200

    feat: support hotspots in iphone by replacing native fullscreen behavior with custom one (#4)

--- src/player-v2/plugin.tsx
+++ src/player-v2/plugin.tsx
@@ -19,6 +19,90 @@ function toObject(
     }
 }
 
+(function shouldEnableLogs() {
+    try {
+        if (document.URL.indexOf("debugKalturaPlayer") !== -1) {
+            enableLog("hotspots");
+        }
+    } catch (e) {
+        // do nothing
+    }
+})();
+
+const WEBKIT_PLAYS_INLINE_KEY = "EmbedPlayer.WebKitPlaysInline";
+
+function isIpad() {
+    return navigator.userAgent.indexOf("iPad") != -1;
+}
+
+function isIphone() {
+    return navigator.userAgent.indexOf("iPhone") != -1 && !isIpad();
+}
+
+function shouldEnableIphoneMode(
+    pluginSupportEnabled: boolean | undefined
+): boolean {
+    const pluginSupportEnabledDefined =
+        typeof pluginSupportEnabled !== "undefined";
+
+    if (
+        !isIphone() ||
+        (pluginSupportEnabledDefined && !pluginSupportEnabled)
+    ) {
+        return false;
+    }
+
+    return true;
+}
+
+(function setupIphoneEnvironment() {
+    try {
+        // @ts-ignore
+        const playerConfig = window.kalturaIframePackageData.playerConfig;
+        const inlineMode = playerConfig.vars[WEBKIT_PLAYS_INLINE_KEY];
+        const pluginSupportEnabled =
+            playerConfig.plugins.hotspots.iphoneFullscreenSupport;
+        const inlineModeDefined = typeof inlineMode !== "undefined";
+
+        log(
+            "debug",
+            "setupIphoneEnvironment",
+            "check conditions needed to setup environment",
+            {
+                inlineMode,
+                inlineModeDefined,
+                pluginSupportEnabled,
+                isIphone: isIphone()
+            }
+        );
+
+        if (
+            inlineModeDefined ||
+            !shouldEnableIphoneMode(pluginSupportEnabled)
+        ) {
+            log(
+                "log",
+                "setupIphoneEnvironment",
+                "setup iphone environment is aborted by configuration (either iphone not detected, inline flag is already set or explicitly configured not to handle iphone fullscreen by plugin configuration)"
+            );
+            return;
+        }
+
+        log(
+            "log",
+            "setupIphoneEnvironment",
+            "setup iphone environment to handle use custom fullscreen handling"
+        );
+        playerConfig.vars[WEBKIT_PLAYS_INLINE_KEY] = true;
+    } catch (e) {
+        log(
+            "error",
+            "setupIphoneEnvironment",
+            `failed to setup iphone environment with error ${e.message}`
+        );
+    }
+})();
+
 mw.kalturaPluginWrapper(function() {
     mw.PluginManager.add(
         "hotspots",
@@ -29,17 +113,8 @@ mw.kalturaPluginWrapper(function() {
             stage: null,
             defaultConfig: {
                 parent: "videoHolder",
+                iphoneFullscreenSupport: undefined, //important don't set it explicitly here to either false or true
                 order: 1
-            },
-
-            shouldEnableLogs() {
-                try {
-                    if (document.URL.indexOf("debugKalturaPlayer") !== -1) {
-                        enableLog("hotspots");
-                    }
-                } catch (e) {
-                    // do nothing
-                }
             },
 
             handleVideoSizeChange: function(e: any) {
@@ -63,7 +138,15 @@ mw.kalturaPluginWrapper(function() {
             },
 
             setup: function() {
-                this.shouldEnableLogs();
+                if (this.enableIphoneFullscreen()) {
+                    log(
+                        "log",
+                        "setup",
+                        "iphone detected, prevent native fullscreen and use inline player"
+                    );
+                    mw.setConfig("EmbedPlayer.ExternalFullScreenControl", true);
+                }
+
                 this.addBindings();
             },
 
@@ -167,8 +250,50 @@ mw.kalturaPluginWrapper(function() {
                 };
             },
 
+            enableIphoneFullscreen: function() {
+                const pluginSupportEnabled = this.getConfig(
+                    "iphoneFullscreenSupport"
+                );
+
+              const inlineMode = this.embedPlayer.getKalturaConfig('', WEBKIT_PLAYS_INLINE_KEY);
+              const inlineModeDefined = typeof inlineMode !== 'undefined';
+
+              const result = shouldEnableIphoneMode(pluginSupportEnabled) && (!inlineModeDefined || inlineMode);
+
+              log('debug', 'plugin.enableIphoneFullscreen', `check if iphone fullscreen is enabled resulted with ${result}`, {inlineMode, inlineModeDefined, pluginSupportEnabled});
+
+              return result;
+            },
+
+            enterFullscreenInIphone: function() {
+                if (!this.enableIphoneFullscreen()) {
+                    return;
+                }
+
+                log(
+                    "log",
+                    "plugin.addBindings[onToggleFullscreen]",
+                    "handle iphone fullscreen toggle manually"
+                );
+                const manager = this.getPlayer().layoutBuilder
+                    .fullScreenManager;
+
+                if (!manager.inFullScreen) {
+                    manager.doContextTargetFullscreen();
+                    manager.inFullScreen = true;
+                    return;
+                }
+
+                manager.restoreContextPlayer();
+                manager.inFullScreen = false;
+            },
+
             addBindings: function() {
                 var _this = this;
+
+                this.bind("onToggleFullscreen", function() {
+                    _this.enterFullscreenInIphone();
+                });
 
                 this.bind("playerReady", function() {
                     const props = {
@@ -194,6 +319,8 @@ mw.kalturaPluginWrapper(function() {
                         _this.stage.showHotspots();
                         _this._wasPlayed = true;
                     }
+
+                    _this.enterFullscreenInIphone();
                 });
 
                 this.bind("seeked", function() {
@@ -208,8 +335,9 @@ mw.kalturaPluginWrapper(function() {
                     _this._wasPlayed = false;
                     _this._videoSize = null;
 
-                    // @ts-ignore
+
                     render(
+                        // @ts-ignore
                         h(null),
                         jQuery('[id="hotspotsOverlay"]')[0],
                         _this._root
