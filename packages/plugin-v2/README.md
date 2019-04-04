688fd90ff392af24f28e302e492e93ff9ec81a05
Eran Sakal <eran@sakalim.com>
2019-04-04 16:16:57 +0300

    support devtool source maps

--- packages/plugin-v2/README.md
+++ packages/plugin-v2/README.md
@@ -7,15 +7,13 @@
 
 ### Deploy plugin to production
 Hotspots plugin is deployed to production as part of [mwEmbed repo](https://github.com/kaltura/mwEmbed). To deploy it do the following:
-1. run `npm run build-v2`
-2. copy `build/bundle.js` into `modules/hotspots/resources/hotspots.js` in [mwEmbed repo](https://github.com/kaltura/mwEmbed).
-3. copy `build/bundle.js.map` into `modules/hotspots/resources/hotspots.js.map` in [mwEmbed repo](https://github.com/kaltura/mwEmbed).
-4. if you changed `src/player-v2/public/hotspots.css` - copy into `modules/hotspots/resources/hotspots.css` in [mwEmbed repo](https://github.com/kaltura/mwEmbed).
-4. test it with the test page `modules/hotspots/tests/hotspots.test.html` (using xamp or mamp). for example `http://localhost:8888/html5.kaltura/mwEmbed/modules/hotspots/tests/hotspots.test.html` 
+1. run `npm run build`
+2. copy `dist/bundle.min.js` into `modules/hotspots/resources/hotspots.js` in [mwEmbed repo](https://github.com/kaltura/mwEmbed).
+3. test it with the test page `modules/hotspots/tests/hotspots.test.html` (using xamp or mamp). for example `http://localhost:8888/html5.kaltura/mwEmbed/modules/hotspots/tests/hotspots.test.html` 
 
 	* if needed you can enable debug logs in console by using a query string `?debugKalturaPlayer` and filtering messages with `[hotspots]` in the console.
-5. once satisfied commit and push changes **in this branch**.
-1. update library version in `package.json` and modify `CHANGELOG.md` file. commit your changes and create a release in github.
+4. once satisfied commit and push changes **in this branch**.
+5. update library version in `package.json` and modify `CHANGELOG.md` file. commit your changes and create a release in github.
 6. update `modules/hotspots/readme.md` in [mwEmbed repo](https://github.com/kaltura/mwEmbed).
 7. commit and push changes in [mwEmbed repo](https://github.com/kaltura/mwEmbed).
 8. go grab a coffee.
--- packages/plugin-v7/webpack.config.js
+++ packages/plugin-v7/webpack.config.js
@@ -1,46 +1,50 @@
-const path = require('path');
+const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyPlugin = require("copy-webpack-plugin");
 
-const distFolder = path.join(__dirname, '/dist');
-module.exports = {
-  entry: './src/index.ts',
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
-    alias: {'@plugin/core': path.resolve(__dirname, '../shared/')},
-    modules: [path.resolve(__dirname, 'node_modules')],
-    symlinks: false
-  },
-  output: {
-    path: distFolder,
-    filename: 'bundle.min.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        loader: 'awesome-typescript-loader'
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: path.resolve(distFolder, "index.html"),
-      template: path.resolve("src", "test.ejs"),
-      inject: false,
-      hash: true
-    }),
-    new CopyPlugin([
-      { from: 'src/tests', to: distFolder }
-    ])
-  ],
-  devServer: {
-    contentBase: distFolder,
-    historyApiFallback: true,
-    hot: false,
-    inline: true,
-    publicPath: "/",
-    index: 'test.html',
-    port: 8007
-  }
+const distFolder = path.join(__dirname, "/dist");
+module.exports = (env, options) => {
+  return {
+    entry: "./src/index.ts",
+    resolve: {
+      extensions: [".ts", ".tsx", ".js"],
+      alias: { "@plugin/core": path.resolve(__dirname, "../shared/") },
+      modules: [path.resolve(__dirname, "node_modules")],
+      symlinks: false
+    },
+    output: {
+      path: distFolder,
+      filename: "bundle.min.js"
+    },
+    devtool: options.mode == "development" ? "source-map" : false,
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          loader: "awesome-typescript-loader"
+        }
+      ]
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        filename: path.resolve(distFolder, "index.html"),
+        template: path.resolve("src", "test.ejs"),
+        inject: false,
+        hash: true
+      }),
+      new CopyPlugin([
+        { from: "src/tests", to: distFolder }
+      ])
+    ],
+    devServer: {
+      contentBase: distFolder,
+      historyApiFallback: true,
+      hot: false,
+      inline: true,
+      publicPath: "/",
+      index: "test.html",
+      port: 8007
+    }
+
+  };
 }
